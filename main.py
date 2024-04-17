909525ecce9f8326b34d6796f10f6a8e2bdf3a71
Thiago Augusto Balko De Faria <129328391+ThiagoFaria2013@users.noreply.github.com>
2024-04-17 20:10:24 +0000

    starting the project

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-def my_function():
-  print("hello")
-  print("bye")
+import random
 
-my_function()
+word_list = ["aardvark", "baboon", "camel"]
+display = []
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+        display += "_"
+print(display)
+
+guess = input('guess a letter: ').lower()
+
+for position in range(word_length):
+        letter = chosen_word[position]            
+        if letter == guess:
+                 display[position] = letter
+print(display)
+
